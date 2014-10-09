e5899728e372aa8b65f1e3ef87f872df93a82669
apartensky <apartensky@gmail.com>
2014-10-09 16:16:20 -0400

    Smoketest with ng+wijmo.wijgrid

--- src/main/webapp/wijmo/wijmoreqng/wijmoreqng-main.js
+++ src/main/webapp/wijmo/wijmoreqng/wijmoreqng-main.js
@@ -11,6 +11,7 @@
 //	        "globalize": "globalize.min"
 //	    }
 //	});
+	
 	function createWijmoCdnPath(wijmolib){
 		return 'http://cdn.wijmo.com/amd-js/3.20142.45/'+wijmolib;
 	}
@@ -29,61 +30,84 @@
 //	    "jquery.mousewheel": createWijmoCdnPath("jquery.mousewheel.min"),
 		"jquery.mousewheel": "//cdnjs.cloudflare.com/ajax/libs/jquery-mousewheel/3.1.11/jquery.mousewheel",
 	    "globalize": createWijmoCdnPath("globalize.min"),
-//	    "ng": ""
+	    "ng": "//ajax.googleapis.com/ajax/libs/angularjs/1.2.12/angular",
+	    "ng-ui-router" :  "//cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.11/angular-ui-router",
+	    "wijmo-ng": "//cdn.wijmo.com/interop/angular.wijmo.3.20142.45"
 	};
-	var addWijmoRef = createWijmoAmdRef.bind(paths);
 	
-	addWijmoRef("wijmo.wijgrid");
-	addWijmoRef("wijmo.widget");
-	addWijmoRef("wijmo.wijutil");
-	addWijmoRef("wijmo.data");
-	addWijmoRef("wijmo.wijpager");
-	addWijmoRef("wijmo.wijinputdate");
-	addWijmoRef("wijmo.wijinputnumber");
-	addWijmoRef("wijmo.wijsuperpanel");
-	addWijmoRef("wijmo.wijinputtext");
-	addWijmoRef("wijmo.wijlist");
-	addWijmoRef("wijmo.wijtouchutil");
-	addWijmoRef("wijmo.wijinputcore");
-	addWijmoRef("wijmo.wijstringinfo");
-	addWijmoRef("wijmo.wijinputnumberformat");
-	addWijmoRef("wijmo.wijinputdateformat");
-	addWijmoRef("wijmo.wijcalendar");
-	addWijmoRef("wijmo.wijtabs");
-	addWijmoRef("wijmo.wijinputdateroller");
-	addWijmoRef("wijmo.wijinpututility");
-	addWijmoRef("wijmo.wijpopup");
-	addWijmoRef("wijmo.wijcharex");
-	addWijmoRef("wijmo.wijinputtextformat");
-	addWijmoRef("wijmo.wijtooltip");	
-        
+	var wijmoRefs = ["wijmo.wijgrid","wijmo.widget","wijmo.wijutil","wijmo.data","wijmo.wijpager","wijmo.wijinputdate","wijmo.wijinputnumber","wijmo.wijsuperpanel","wijmo.wijinputtext","wijmo.wijlist","wijmo.wijtouchutil","wijmo.wijinputcore","wijmo.wijstringinfo","wijmo.wijinputnumberformat","wijmo.wijinputdateformat","wijmo.wijcalendar","wijmo.wijtabs","wijmo.wijinputdateroller","wijmo.wijinpututility","wijmo.wijpopup","wijmo.wijcharex","wijmo.wijinputtextformat","wijmo.wijtooltip"];
+	var addWijmoRef = createWijmoAmdRef.bind(paths);
+	for(var i=0;i<wijmoRefs.length;i++){
+		addWijmoRef(wijmoRefs[i]);
+	}
+//	addWijmoRef("wijmo.wijgrid");
+//	addWijmoRef("wijmo.widget");
+//	addWijmoRef("wijmo.wijutil");
+//	addWijmoRef("wijmo.data");
+//	addWijmoRef("wijmo.wijpager");
+//	addWijmoRef("wijmo.wijinputdate");
+//	addWijmoRef("wijmo.wijinputnumber");
+//	addWijmoRef("wijmo.wijsuperpanel");
+//	addWijmoRef("wijmo.wijinputtext");
+//	addWijmoRef("wijmo.wijlist");
+//	addWijmoRef("wijmo.wijtouchutil");
+//	addWijmoRef("wijmo.wijinputcore");
+//	addWijmoRef("wijmo.wijstringinfo");
+//	addWijmoRef("wijmo.wijinputnumberformat");
+//	addWijmoRef("wijmo.wijinputdateformat");
+//	addWijmoRef("wijmo.wijcalendar");
+//	addWijmoRef("wijmo.wijtabs");
+//	addWijmoRef("wijmo.wijinputdateroller");
+//	addWijmoRef("wijmo.wijinpututility");
+//	addWijmoRef("wijmo.wijpopup");
+//	addWijmoRef("wijmo.wijcharex");
+//	addWijmoRef("wijmo.wijinputtextformat");
+//	addWijmoRef("wijmo.wijtooltip");	
+    
+	
 	requirejs.config({
-            paths: paths
+            paths: paths,
+            shim: {
+            	ng: {
+            		deps: ["jquery"],
+            		exports: "angular"
+            	},
+            	"ng-ui-router": {
+            		deps: ["ng"]
+            	},
+            	"wijmo-ng": {
+            		deps: ["ng"].concat(wijmoRefs)
+            	}
+            }
     });
 	
-	requirejs(["wijmo.wijgrid"], function () {
-	    
-	    $(document).ready(function () {
-	        $("#demo-grid").wijgrid({
-	            allowSorting: true,
-	            allowPaging: true,
-	            pageSize: 3,
-	            data: [
-	                [27, 'Canada', 'Adams, Craig', 'RW'],
-	                [43, 'Canada', 'Boucher, Philippe', 'D', 'R'],
-	                [24, 'Canada', 'Cooke, Matt', 'LW', 'L'],
-	                [87, 'Canada', 'Crosby, Sidney (C)', 'C', 'L'],
-	                [1, 'United States', 'Curry, John', 'G', 'L'],
-	            ],
-	            columns: [
-	                {headerText: "Number"},
-	                {headerText: "Nationality"},
-	                {headerText: "Player"},
-	                {headerText: "Position"}
-	            ]
-	        });
-	    });
+	requirejs(["ng", "wijmoreqng-app"], function (ng, app) {
+//	requirejs(["wijmo.wijgrid"], function () {	    
+//	    $(document).ready(function () {
+//	        $("#demo-grid").wijgrid({
+//	            allowSorting: true,
+//	            allowPaging: true,
+//	            pageSize: 3,
+//	            data: [
+//	                [27, 'Canada', 'Adams, Craig', 'RW'],
+//	                [43, 'Canada', 'Boucher, Philippe', 'D', 'R'],
+//	                [24, 'Canada', 'Cooke, Matt', 'LW', 'L'],
+//	                [87, 'Canada', 'Crosby, Sidney (C)', 'C', 'L'],
+//	                [1, 'United States', 'Curry, John', 'G', 'L'],
+//	            ],
+//	            columns: [
+//	                {headerText: "Number"},
+//	                {headerText: "Nationality"},
+//	                {headerText: "Player"},
+//	                {headerText: "Position"}
+//	            ]
+//	        });
+//	    });
 		
+		ng.element(document).ready(function(){
+			console.debug("app", app);
+			ng.bootstrap(document, [app.name]);
+		});
 		
 	  });
 	
