0ec484d1f0ebd678ce886a3edcfe5830d6922e85
apartensky <apartensky@gmail.com>
2014-11-12 16:22:12 -0500

    Added plaything for resolving promises inside the model

--- src/main/webapp/ngboostrap/app/junk/userModel.js
+++ src/main/webapp/ngboostrap/app/junk/userModel.js
@@ -1,3 +1,4 @@
+//resolve promises in controller (view model)
 function LoginController($scope, $timeout) {
     $scope.user=new User();
     getUser().then(function(data){
@@ -29,6 +30,44 @@ function LoginController($scope, $timeout) {
             return this._raw.address.city;
         //else
         //    return "";
+    }
+}
+
+//resolve promises in the model
+function LoginController($scope, $timeout) {
+    $scope.user=getUser();
+    
+    $scope.logins = [];
+    $scope.login = function () {
+        console.log("User:", $scope.user.getCity());
+    };
+    $scope.$watch("user.getCity()", function(oldVal, newVal){
+        console.log("watch:", oldVal, newVal);
+    });
+    function getUser(){
+        return new User($timeout(function(){
+            return {
+                    firstName: "Foo",
+                    lastName: "Bar",
+                    address: {
+                        //city: "Boston"
+                    }
+                };
+        }, 1000));
+    }    
+    function User(promise){
+        var self=this;
+        self.promise=promise.then(function(raw){
+            self.resolved=true;
+            self._raw=raw;
+        });
+    };
+    
+    User.prototype.getCity=function(){
+        if(this.resolved)
+            return this._raw.address.city;
+        else
+            return "";
     }
 }
 
