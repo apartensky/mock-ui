41725bbbacb75d06a1ac88ce8f35564a00b9f0ff
apartensky <apartensky@gmail.com>
2014-12-26 15:08:57 -0500

    Toggle collapse caret icon on panel open/close

--- src/main/webapp/ngboostrap/app/widgets/common/panel/widgets.common.panel.module.js
+++ src/main/webapp/ngboostrap/app/widgets/common/panel/widgets.common.panel.module.js
@@ -1,7 +1,7 @@
 define(["ng", "./directives/panel.directive", "./controllers/PanelVM", "./services/panelSrvc"], 
-function(ng, SidemenuDirective, PanelVM, PanelSrvc){
+function(ng, PanelDirective, PanelVM, PanelSrvc){
 	var module=ng.module("mui.widgets.common.panel", []);
-	module.directive("panel", SidemenuDirective);	
+	module.directive("panel", PanelDirective);	
 	module.controller("PanelVM", PanelVM);	
 	module.service("PanelSrv", PanelSrvc);
 	return module;
